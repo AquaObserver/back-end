672b7f25307e586618eb35e59e96ae2e6ad9fe32
kbaljak <karlo.baljak@fer.hr>
2023-12-03 16:13:56 +0100

    .

--- AquaObserver_api/serializers.py
+++ AquaObserver_api/serializers.py
@@ -1,1 +1,9 @@
 #for transforming python objects to JSON
+from rest_framework import serializers
+from .models import DeviceReadings
+
+
+class ReadingSerializer(serializers.ModelSerializer):
+    class Meta:     #metadata describing the model
+        model = DeviceReadings
+        fields = ['id', 'tstz', 'deviceId', 'waterLevel']
