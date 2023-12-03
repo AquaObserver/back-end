6aa82dcaff49bad65e2d0774b9d49ef4e317dfa0
kbaljak <karlo.baljak@fer.hr>
2023-12-03 16:35:42 +0100

    .

--- AquaObserver_api/views.py
+++ AquaObserver_api/views.py
@@ -4,11 +4,11 @@ from django.http import JsonResponse
 from .models import DeviceReadings
 from .serializers import ReadingSerializer
 
-#defined readings endpoint aka. /readings/
+#defined readings endpoint aka /readings/
 def readingsList(request):
     #get all the readings
     retrievedReadings = DeviceReadings.objects.all()
     #serialize readings
     serializer = ReadingSerializer(retrievedReadings, many=True)
     #return json
-    return JsonResponse(serializer.data)
+    return JsonResponse({"readings": serializer.data})
