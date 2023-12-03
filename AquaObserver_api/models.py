66bd974a72fc946ba13bd8c9184177fd4077ea36
kbaljak <karlo.baljak@fer.hr>
2023-12-03 16:01:01 +0100

    Created basic model for DB

--- AquaObserver_api/models.py
+++ AquaObserver_api/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 #defining a model of data that will be stored in the database aka. defining the table
-class DeviceReadings(models.Modle):
+class DeviceReadings(models.Model):
     # no need to ad entry ID, it is added automaticaly
     tstz = models.DateTimeField()       #timestamp timezone aka. YYYY-MM-DD HH:MM:SS.SSS for then the reading was taken
     deviceId = models.IntegerField()    #ID of the device that took the readings
-    waterLevel = models.FloatField()    #
+    waterLevel = models.FloatField()    #level of the water measured from the device
