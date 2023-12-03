06a7100eea20732c01c80df0356b9588a23ea33a
kbaljak <karlo.baljak@fer.hr>
2023-12-03 16:48:52 +0100

    .

--- AquaObserver_api/views.py
+++ AquaObserver_api/views.py
@@ -3,12 +3,25 @@
 from django.http import JsonResponse
 from .models import DeviceReadings
 from .serializers import ReadingSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-#defined readings endpoint aka /readings/
+#defined readings endpoint aka readings/
+@api_view(['GET','POST'])
 def readingsList(request):
-    #get all the readings
-    retrievedReadings = DeviceReadings.objects.all()
-    #serialize readings
-    serializer = ReadingSerializer(retrievedReadings, many=True)
-    #return json
-    return JsonResponse({"readings": serializer.data})
+
+    #front-end is retreiving data
+    if request.method == 'GET':
+        #get all the readings
+        retrievedReadings = DeviceReadings.objects.all()
+        #serialize readings
+        serializer = ReadingSerializer(retrievedReadings, many=True)
+        #return json
+        return JsonResponse({"readings": serializer.data})
+    #hardware is sending data
+    if request.method == 'POST':
+        serializer = ReadingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()       #saves it to the DB
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
