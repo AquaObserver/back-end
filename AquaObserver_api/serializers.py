5d6d650d3428bdff82306fc6fc4dbd736dda0a0f
kbaljak <karlo.baljak@fer.hr>
2023-12-06 19:59:16 +0100

    added serializer for UserThreshold table

--- AquaObserver_api/serializers.py
+++ AquaObserver_api/serializers.py
@@ -1,9 +1,15 @@
 #for transforming python objects to JSON
 from rest_framework import serializers
 from .models import DeviceReadings
+from .models import UserThreshold
 
 
 class ReadingSerializer(serializers.ModelSerializer):
     class Meta:     #metadata describing the model
         model = DeviceReadings
         fields = ['id', 'tstz', 'deviceId', 'waterLevel']
+
+class ThresholdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserThreshold
+        fields = ['id', 'userId', 'thresholdLevel']
