4a9f2e09655fcd6df5b17e5dc6f08ed36ad1bc86
kbaljak <karlo.baljak@fer.hr>
2023-12-06 20:00:03 +0100

    new rout for user threshold

--- AquaObserver_api/urls.py
+++ AquaObserver_api/urls.py
@@ -21,5 +21,7 @@ from AquaObserver_api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('readings/', views.readingsList),
-    path('dailyLatest/', views.getLatestDaily)
+    path('dailyLatest/', views.getLatestDaily),
+    path('userThreshold/<int:userID>', views.userThreshold),
+    path('userThreshold/', views.userThreshold)
 ]
--- AquaObserver_api/views.py
+++ AquaObserver_api/views.py
@@ -3,7 +3,9 @@ import json
 from django.db.models import Q
 from django.http import JsonResponse
 from .models import DeviceReadings
+from .models import UserThreshold
 from .serializers import ReadingSerializer
+from .serializers import ThresholdSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -30,10 +32,38 @@ def readingsList(request):
 #define latest daily reading endpoint aka dailyLatest/
 def getLatestDaily(request):
     if request.method == 'GET':
-        requestedDate = json.loads(request.body).get('date')
+        requestedDate = json.loads(request.body).get('date')    #from JSON get value from filed "data":
         try:
             lastReading = DeviceReadings.objects.filter(Q(tstz__startswith=requestedDate)).latest('tstz')   #would translate to query: Select * from DeviceReadings where tstz like "<requestedDate>%" and gets the latest reading
         except DeviceReadings.DoesNotExist:
             return JsonResponse({"waterLevel": -1}) #in case there is no record in database
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     return JsonResponse({"waterLevel": lastReading.waterLevel})
+
+#defined userThreshold endpoint aka userThreshold/ or userThreshold/<int:userID>
+@api_view(['GET', 'POST', 'PUT'])
+def userThreshold(request, userID=None):
+    if request.method == 'GET':
+        try:
+            user = UserThreshold.objects.get(userId=userID)
+        except UserThreshold.DoesNotExist:
+            return JsonResponse({"user": None, "threhsold": -1})
+        return JsonResponse({"user": user.userId, "threhsold": user.thresholdLevel})
+
+    if request.method == 'POST':
+        serializer = ThresholdSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     
+    if request.method == 'PUT':
+        try:
+            user = UserThreshold.objects.get(userId=userID)
+            user.delete()
+        except UserThreshold.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = ThresholdSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
